915fef77843839847748133cfefab9dc6c124b9a
speadup <speadup@163.com>
2015-06-17 20:06:48 +0800

    1.调整目录

--- enhance/luci-app-data/luasrc/controller/data.lua
+++
@@ -1,9 +0,0 @@
-module("luci.controller.data",package.seeall)
-function index()
-    local e
-    e = entry({"admin","data"},alias("admin","data","data"),_("Data"),70)
-    e.index=true
-    e.sysauth = {"root", "debug"}
-    e = entry({"admin","data","data"},cbi("data"),_("Data"))
-    e.dependent=true
-end
--- enhance/luci-app-settings/luasrc/controller/settings.lua
+++
@@ -1,9 +0,0 @@
-module("luci.controller.settings",package.seeall)
-function index()
-    local e
-    e = entry({"admin","settings"},alias("admin","settings","settings"),_("Settings"),60)
-    e.index=true
-    e.sysauth = {"root", "debug"}
-    e = entry({"admin","settings","settings"},cbi("settings"),_("Settings"))
-    e.dependent=true
-end

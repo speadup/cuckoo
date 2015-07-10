41683f3fb19dcf4cf9a227362e9d36779a8fcb14
speadup <speadup@163.com>
2015-07-10 14:09:41 +0800

    1.删除一些无用的软件包

--- plus/hpanalysis/files/data_hpanalysis.sql
+++
@@ -1,3 +0,0 @@
-insert or ignore into "t_place" values(1,'37020221000000','1111',null,null,null,null,null,null,null,null,null,'flashcardcf11001877',null,null,null,null,'192.168.19.21','2014-02-19 10:54:40',null,null,null,null,null,null,null,null,null,null,null,1);
-insert or ignore into "t_protocol_all" values(18,'3','新浪-WEB邮件','0006','000600000005','Mail',2,1,1);
-insert or ignore into "t_protocol_main" values(2,61,'000600000005',3,'00060000000530000001',1);

a64337c38bd4cc17817ddfe7c090b13203790cb5
speadup <speadup@163.com>
2015-07-10 14:15:38 +0800

    1.增加软件包

---
+++ plus/luci-app-plus/luasrc/controller/plus.lua
@@ -0,0 +1,9 @@
+module("luci.controller.settings",package.seeall)
+function index()
+    local e
+    e = entry({"admin","settings"},alias("admin","settings","settings"),_("Settings"),60)
+    e.index=true
+    e.sysauth = {"root", "debug"}
+    e = entry({"admin","settings","settings"},cbi("plus"),_("Settings"))
+    e.dependent=true
+end

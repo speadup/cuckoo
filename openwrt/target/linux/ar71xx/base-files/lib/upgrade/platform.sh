c5152ca79b8fa82e96c060d57a8bfdec2c5d56d2
speadup <speadup@163.com>
2015-10-22 09:51:30 +0800

    1.同步更新代码 %openwrt@branches/chaos_calmer:47243 %packages@for-15.05:f690a754225b15d71aa162bab07ed15e02a8fbdb %luci@for-15.05:ce5c787989701eb406162fa467e43f034cbbf292 %routing@for-15.05:e870c3373eea80df852d42fac3f40aaffd7a0f58 %oldpackages@master:7a333896ddb38e4545fb6c687ea40de77b0bf970

--- openwrt/target/linux/ar71xx/base-files/lib/upgrade/platform.sh
+++ openwrt/target/linux/ar71xx/base-files/lib/upgrade/platform.sh
@@ -248,7 +248,8 @@ platform_check_image() {
 	nbg460n_550n_550nh | \
 	unifi | \
 	unifi-outdoor | \
-	carambola2 )
+	carambola2 | \
+	weio )
 		[ "$magic" != "2705" ] && {
 			echo "Invalid image type."
 			return 1
@@ -312,6 +313,7 @@ platform_check_image() {
 	el-mini | \
 	gl-inet | \
 	mc-mac1200r | \
+	minibox-v1 |\
 	onion-omega | \
 	oolite | \
 	smart-300 | \

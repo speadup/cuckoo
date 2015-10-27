04a34edd9061ce9931454f56706c405f2e603ec4
speadup <speadup@163.com>
2015-10-27 15:17:08 +0800

    1.修正自动更新补丁脚本

--- utils/update-patch.sh
+++ utils/update-patch.sh
@@ -5,14 +5,14 @@ if [ -e .svn ];then
     while read f name
     do
         [ -d "$name" ] && continue
-        svn diff $name |sed -e '/^--- \|^+++ /s/\s*(.*)$//g' -e '/^--- \|^+++ /s/ *//g' -e '/^Index: /,2d' > patches/${name//\//_}.patch
+        svn diff $name |sed -e '/^--- \|^+++ /s/\s*(.*)$//g' -e '/^--- \|^+++ /s/ *//g' > patches/${name//\//_}.patch
     done
 elif [ -e .git ];then
     git status $path | grep '^#\s*modified:\|^A' | grep ' openwrt\| feeds' | 
     while read f f name
     do
         [ -d "$name" ] && continue
-        git diff --no-prefix $name |sed -e '/^--- \|^+++ /s/(.*)//g' -e '/^--- \|^+++ /s/ *//g' -e '/^diff --git/,2d' > patches/${name//\//_}.patch
+        git diff --no-prefix $name |sed -e '/^--- \|^+++ /s/(.*)//g' -e '/^--- \|^+++ /s/ *//g' > patches/${name//\//_}.patch
     done
 
 else
